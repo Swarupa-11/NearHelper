959851cc5e3c15db2d402f3ef7ad859e60a21bf9
Abhishek <abhishek@example.com>
2026-03-15 22:02:29 +0530

    Update workfinder-auth URL

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 const CONFIG = {
   WORKER_AUTH_URL:        'https://nearhelper-production.up.railway.app',
   WORKER_DASHBOARD_URL:   'https://mellow-manifestation-production-b313.up.railway.app',
-  WORKFINDER_AUTH_URL:    'https://thorough-warmth-production-763e.up.railway.app',
+  WORKFINDER_AUTH_URL:    'https://workfinder-auth-production.up.railway.app',
   WORKFINDER_DASH_URL:    'https://striking-commitment-production-5451.up.railway.app',
 };
